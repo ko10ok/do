e7d14b38debed91df53af798d9056024868675ac
Yuriy Sagitov <pro100.ko10ok@gmail.com>
2025-09-22 13:01:41 +0300

    fix unconsistents info

--- doq/__init__.py
+++ doq/__init__.py
@@ -1,6 +1,6 @@
 """DOQ - Command line interface for various LLM providers."""
 
-__version__ = "1.0.0"
+__version__ = "0.0.1"
 __author__ = "Yuriy Sagitov"
-__email__ = "home_r@mail.ru"
+__email__ = "pro100.ko10ok@gmail.com"
 __license__ = "Apache-2.0"
